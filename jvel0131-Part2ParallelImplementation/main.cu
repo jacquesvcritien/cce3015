329261156e5601c9ecc176f2d3a4a2dafe0423d9
Jacques Vella Critien <vellacritien@gmail.com>
2021-05-17 21:22:37 +0200

    Changed to 64 threads per block to compare speeds - TESTING

--- jvel0131-Part2ParallelImplementation/main.cu
+++ jvel0131-Part2ParallelImplementation/main.cu
@@ -143,7 +143,7 @@ int main(int argc, char *argv[])
 
 	free(a);
 
-	int threadsInBlocks = 32;
+	int threadsInBlocks = 64;
 	const int nblocks = (rows + (threadsInBlocks-1)) / threadsInBlocks;
 	calculateColumnSums<<<nblocks, 64>>>(rows, cols, dii, da);
 	calculateRowSums<<<nblocks, 64>>>(rows, cols, dii, da);
