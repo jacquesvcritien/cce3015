3198ba2c9f71e926ee5a7206d4a52554e7fc97fb
Jacques Vella Critien <vellacritien@gmail.com>
2021-05-26 00:00:03 +0200

    Checking ipc when reducing number of threads

--- jvel0131-Part2ParallelOptimization2/main.cu
+++ jvel0131-Part2ParallelOptimization2/main.cu
@@ -137,7 +137,7 @@ int main(int argc, char *argv[])
 	totalTime += t;
 
 
-	int threadsInBlocks = 128;
+	int threadsInBlocks = 64;
 	const int nblocks = (rows + (threadsInBlocks-1)) / threadsInBlocks;
 	printf("Number of threads in blocks: %d\n", threadsInBlocks);
 	printf("Number of blocks: %d\n", nblocks);
